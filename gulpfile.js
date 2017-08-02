403359d2bb904f4f784b4d7c399c95e40a5e72da
Niklas Higi <niklas@shroudedcode.com>
2017-08-02 23:08:48 +0200

    Setup Sass compilation

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
 const gulp = require('gulp')
+const rename = require('gulp-rename')
 const typescript = require('gulp-typescript')
 const pug = require('gulp-pug')
+const sass = require('gulp-sass')
 
-gulp.task('default', ['typescript', 'pug'])
+gulp.task('default', ['typescript', 'pug', 'sass'])
 
 gulp.task('typescript', () => {
   return gulp.src('./src/**/*.ts')
@@ -16,4 +18,11 @@ gulp.task('pug', () => {
   return gulp.src('./src/**/*.pug')
     .pipe(pug())
     .pipe(gulp.dest('./dist'))
+})
+
+gulp.task('sass', () => {
+  return gulp.src('./src/ui/styles/main.scss')
+    .pipe(sass())
+    .pipe(rename('styles.css'))
+    .pipe(gulp.dest('./dist/ui'))
 })
