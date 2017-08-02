aaf9e40da2153a85a5680e4f5575fce540bd9f6c
Niklas Higi <niklas@shroudedcode.com>
2017-08-02 23:01:17 +0200

    Add Pug compilation

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,19 @@
 const gulp = require('gulp')
 const typescript = require('gulp-typescript')
+const pug = require('gulp-pug')
 
-gulp.task('default', ['typescript'])
+gulp.task('default', ['typescript', 'pug'])
 
 gulp.task('typescript', () => {
   return gulp.src('./src/**/*.ts')
     .pipe(typescript({
       noImplicitAny: true
     }))
+    .pipe(gulp.dest('./dist'))
+})
+
+gulp.task('pug', () => {
+  return gulp.src('./src/**/*.pug')
+    .pipe(pug())
     .pipe(gulp.dest('./dist'))
 })
