283b909165ee6a7d2a8224768753ece8f40ea333
Niklas Higi <niklas@shroudedcode.com>
2017-08-03 22:39:09 +0200

    Setup basic Electron boilerplate

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import { app, BrowserWindow } from 'electron'
 import path = require('path')
 
+import { TestIO } from './io/test-io'
 let win
+
+global.io = new TestIO()
 
 app.on('ready', () => {
   win = new BrowserWindow({
