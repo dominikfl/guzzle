7aa0e094868bb7e286675ba7bab2bd978c45e3e8
Niklas Higi <niklas@shroudedcode.com>
2017-08-03 22:39:55 +0200

    Create `Liquid` class

---
+++ src/models/liquid.ts
@@ -0,0 +1,12 @@
+export class Liquid {
+
+  /** The name of the liquid. */
+  name: string
+
+  /** The density of the liquid. */
+  density: number
+
+  /** Whether the liquid is alcoholic. */
+  alcoholic: boolean
+
+}
