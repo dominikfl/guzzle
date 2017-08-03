90ed91f0cbd78cdb605914e7b908ead926244a92
Niklas Higi <niklas@shroudedcode.com>
2017-08-03 22:39:44 +0200

    Create `MixingJob` class

---
+++ src/models/mixing-job.ts
@@ -0,0 +1,41 @@
+import { Job } from './job'
+import { MixingStep } from './mixing-step'
+import { Drink } from './drink'
+
+/** The job implementation that handles mixing drinks. */
+export class MixingJob implements Job {
+
+  /** The drink being mixed by the job. */
+  drink: Drink
+
+  /** The steps the job is handling. */
+  steps: Array<MixingStep>
+
+  constructor(drink: Drink) {
+    this.steps = drink.steps
+  }
+
+  /** The step the job is currently working on. **/
+  getCurrentStep() {
+    return this.steps[0]
+  }
+
+  tick(io) {
+    if(this.getCurrentStep().execute()) this.steps.shift()
+  }
+
+  getProgress() {
+    const totalSteps = this.drink.steps.length
+    const doneSteps = totalSteps - this.steps.length
+    return doneSteps / totalSteps
+  }
+
+  getTitle() {
+    return `Mixing ${this.drink.name} ...`
+  }
+
+  getDescription() {
+    return this.getCurrentStep().getDescription()
+  }
+
+}
