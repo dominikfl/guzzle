59ba4fcfc305a07f9f130c277f207f80929e1bf4
Niklas Higi <niklas@shroudedcode.com>
2017-08-22 18:24:57 +0200

    Create constructors for `Liquid` and `Drink`

--- src/models/drink.ts
+++ src/models/drink.ts
@@ -3,6 +3,16 @@ import { PouringStep } from './pouring-step'
 
 export class Drink {
 
+  constructor(name: string,
+              description: string,
+              color: string,
+              steps: Array<MixingStep>) {
+    this.name = name
+    this.description = description
+    this.color = color
+    this.steps = steps
+  }
+
   /** The displayed name of the drink. */
   name: string
 
--- src/models/liquid.ts
+++ src/models/liquid.ts
@@ -1,5 +1,10 @@
 export class Liquid {
 
+  constructor(name: string, density: number) {
+    this.name = name
+    this.density = density
+  }
+
   /** The name of the liquid. */
   name: string
 
