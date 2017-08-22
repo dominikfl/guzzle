e5c244510d96b0e720ff31158937b5f0269a6fa7
Niklas Higi <niklas@shroudedcode.com>
2017-08-22 18:11:08 +0200

    Make navigation buttons do something

--- src/ui/app.ts
+++ src/ui/app.ts
@@ -6,9 +6,23 @@ const app = new Vue({
     view: 'drink',
   },
   computed: {
+    backButtonHidden() {
+      return this.view !== 'drink'
+    },
+    exitButtonHidden() {
+      return this.view === 'home'
+    },
     themeColor() {
       if(this.view === 'drink') return '#F9C22C'
       return '#76c455'
+    }
+  },
+  methods: {
+    goBack() {
+      this.view = 'home'
+    },
+    exit() {
+      this.view = 'home'
     }
   },
 })
