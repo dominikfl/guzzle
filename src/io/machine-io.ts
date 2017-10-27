adcbcb557bf879fb9b7ee280a0d35fd8b61c6594
Niklas Higi <niklas@shroudedcode.com>
2017-10-27 16:46:35 +0200

    Translate HX711 C library to JavaScript

--- src/io/machine-io.ts
+++ src/io/machine-io.ts
@@ -33,8 +33,33 @@ export class MachineIO implements IO {
   }
 
   async getScaleWeight() {
-    // TODO: Read weight from HX711 sensor
-    return 0
+    /*
+      This code was taken from https://github.com/bogde/HX711
+      and translated from C into JavaScript.
+     */
+
+  	let data: number[]
+  	let filler = 0x00
+
+  	// Pulse the clock pin 24 times to read the data
+    const scale = this.config.scale
+  	data[2] = await this.shiftIn(scale.dataPin, scale.clockPin, 0)
+  	data[1] = await this.shiftIn(scale.dataPin, scale.clockPin, 0)
+  	data[0] = await this.shiftIn(scale.dataPin, scale.clockPin, 0)
+
+  	// Set the channel for the next reading
+    gpio.write(scale.clockPin, rpiGpio.DIR_HIGH)
+    gpio.write(scale.clockPin, rpiGpio.DIR_LOW)
+
+  	// Replicate the most significant bit to pad out a 32-bit signed integer
+  	if (data[2] & 0x80) {
+  		filler = 0xFF
+  	} else {
+  		filler = 0x00
+  	}
+
+  	// Construct a 32-bit signed integer
+  	return (filler << 24 | data[2] << 16 | data[1] << 8 | data[0])
   }
 
   /** Shifts in a byte of data one bit at a time. */
